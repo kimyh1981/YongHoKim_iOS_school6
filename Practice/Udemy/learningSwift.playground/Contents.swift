db0df5ea8dd5bba15c1f97aa026de438f79b841d
Kim Yong Ho <kimyh1981@gmail.com>
2018-01-14 23:40:49 +0900

    practiceAdded

---
+++ Practice/Udemy/learningSwift.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello"
+
+str
+
+str = "hello, world"
+
+str
+
+var number = 7
+
+number
+number
+
+number = 10
+
+str = 20
+var str = ""
+
+str = "hello, apple"
+
+number
+
+
