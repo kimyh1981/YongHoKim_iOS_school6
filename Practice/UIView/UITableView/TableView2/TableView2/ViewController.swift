5ac8347c0cefc3de5a70e960c2637a3b556e5836
Kim Yong Ho <kimyh1981@gmail.com>
2018-02-23 02:18:28 +0900

    addedUITableViewSampleCompleted.

--- Practice/UIView/UITableView/TableView2/TableView2/ViewController.swift
+++ Practice/UIView/UITableView/TableView2/TableView2/ViewController.swift
@@ -26,35 +26,62 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell: MyTableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyTableViewCell
         if indexPath.row == 0 {
-            cell.sunny1.image = UIImage(named: "sunny.jpg")
-            cell.time1.text = "오전 03:50"
-            cell.city1.text = "부산"
-            cell.temp1.text = "-5"
+            cell.sunny1.image = UIImage(named: "sunny2.jpg")
+            cell.time1.text = "AM 03:50"
+            cell.city1.text = "Seoul"
+            cell.temp1.text = "-5 ℃"
         } else if indexPath.row == 1 {
-            cell.sunny1.image = UIImage(named: "rainy.jpg")
+            cell.sunny1.image = UIImage(named: "rainy2.jpg")
+            cell.time1.text = "AM 08:50"
+            cell.city1.text = "London"
+            cell.temp1.text = "-2 ℃"
         } else if indexPath.row == 2 {
-            cell.sunny1.image = UIImage(named: "cloudy.jpg")
+            cell.sunny1.image = UIImage(named: "cloudy2.jpg")
+            cell.time1.text = "AM 09:50"
+            cell.city1.text = "Mexico City"
+            cell.temp1.text = "23 ℃"
         } else if indexPath.row == 3 {
-            cell.sunny1.image = UIImage(named: "snowy.jpg")
+            cell.sunny1.image = UIImage(named: "snowy2.jpg")
+            cell.time1.text = "PM 03:50"
+            cell.city1.text = "Los Angeles"
+            cell.temp1.text = "19 ℃"
         } else if indexPath.row == 4 {
-            cell.sunny1.image = UIImage(named: "snowy.jpg")
+            cell.sunny1.image = UIImage(named: "sunny2.jpg")
+            cell.time1.text = "AM 08:50"
+            cell.city1.text = "NewYork"
+            cell.temp1.text = "-5 ℃"
         } else if indexPath.row == 5 {
-            cell.sunny1.image = UIImage(named: "snowy.jpg")
+            cell.sunny1.image = UIImage(named: "rainy2.jpg")
+            cell.time1.text = "PM 03:50"
+            cell.city1.text = "Paris"
+            cell.temp1.text = "4 ℃"
         } else if indexPath.row == 6 {
-            cell.sunny1.image = UIImage(named: "snowy.jpg")
+            cell.sunny1.image = UIImage(named: "snowy2.jpg")
+            cell.time1.text = "PM 03:50"
+            cell.city1.text = "Abudabi"
+            cell.temp1.text = "28 ℃"
         } else if indexPath.row == 7 {
-            cell.sunny1.image = UIImage(named: "cloudy.jpg")
+            cell.sunny1.image = UIImage(named: "sunny2.jpg")
+            cell.time1.text = "AM 11:50"
+            cell.city1.text = "Shanghai"
+            cell.temp1.text = "11 ℃"
         } else if indexPath.row == 8 {
-            cell.sunny1.image = UIImage(named: "cloudy.jpg")
+            cell.sunny1.image = UIImage(named: "rainy2.jpg")
+            cell.time1.text = "PM 03:50"
+            cell.city1.text = "Berlin"
+            cell.temp1.text = "-5 ℃"
         } else if indexPath.row == 9 {
-            cell.sunny1.image = UIImage(named: "cloudy.jpg")
+            cell.sunny1.image = UIImage(named: "cloudy2.jpg")
+            cell.time1.text = "AM 04:50"
+            cell.city1.text = "Sydney"
+            cell.temp1.text = "20 ℃"
         } else if indexPath.row == 10 {
-            cell.sunny1.image = UIImage(named: "cloudy.jpg")
+            cell.sunny1.image = UIImage(named: "sunny2.jpg")
         }
         return cell
     }
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
+        return 80
     }
     
     
