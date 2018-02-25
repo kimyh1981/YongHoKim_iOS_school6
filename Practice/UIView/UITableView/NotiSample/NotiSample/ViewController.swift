7a72cc22fba6dc48b88167d937c8aa9c3f4b0ad1
Kim Yong Ho <kimyh1981@gmail.com>
2018-02-25 23:20:58 +0900

    addedPractoceOfNotification20180225.

--- Practice/UIView/UITableView/NotiSample/NotiSample/ViewController.swift
+++ Practice/UIView/UITableView/NotiSample/NotiSample/ViewController.swift
@@ -8,9 +8,22 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+
 
     @IBOutlet weak var lb: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
