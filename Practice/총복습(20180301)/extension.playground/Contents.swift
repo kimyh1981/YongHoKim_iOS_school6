59b089994de113e7b4ff6d77dbff2eefbc8b2c58
Kim Yong Ho <kimyh1981@gmail.com>
2018-03-07 23:47:36 +0900

    addedPracticeOfExtension20180307.

--- Practice/총복습(20180301)/extension.playground/Contents.swift
+++ Practice/총복습(20180301)/extension.playground/Contents.swift
@@ -27,3 +27,36 @@ func oddEvenNum(num: Int) -> Bool {
     
 }
 oddEvenNum(num: 121)
+
+extension Double {
+    var km: Double { return self * 1000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1000.0 }
+    var ft: Double { return self / 3.28084 }
+}
+
+let oneInch = 25.4.mm
+print("one inch is \(oneInch) meters." )
+let threeFeet = 3.ft
+print("Three feet is \(threeFeet) meters.")
+
+extension Int {
+    func repetitions(task: () -> Void) {
+        for _ in 0..<self {
+            task()
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
