7410c114a6d00c41ef68323fdaefbc8b8c3bd0f2
Kim Yong Ho <kimyh1981@gmail.com>
2018-03-16 23:46:40 +0900

    Cocoapod&CarthageFrameworkInstall20180316.

---
+++ Practice/Cocopods/AlamofireSampleTest/AlamofireSampleTest/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AlamofireSampleTest
+//
+//  Created by KimYong Ho on 2018. 3. 16..
+//  Copyright © 2018년 KimYong Ho. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
