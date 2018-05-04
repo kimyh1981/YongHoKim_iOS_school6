b02edbf169eafd3fbaeab6cf85b9b502805318c7
Kim Yong Ho <kimyh1981@gmail.com>
2018-05-04 23:59:56 +0900

    Add practice of property.

---
+++ UdemyCoursePractice/1. Programming in swift/5. Structures/PropertyPractice/PropertyPractice/main.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  PropertyPractice
+//
+//  Created by KimYong Ho on 04/05/2018.
+//  Copyright © 2018 KimYH. All rights reserved.
+//
+
+import Foundation
+
+struct Person {
+    var firstName: String
+    var lastName: String
+}
+
+var person = Person(firstName: "YH", lastName: "Kim")
