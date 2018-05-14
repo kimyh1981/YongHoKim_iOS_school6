18b863390e7efe67d8365f33fb8b43fe671875ff
Kim Yong Ho <kimyh1981@gmail.com>
2018-05-14 23:57:24 +0900

    Add Class VS Struct

---
+++ UdemyCoursePractice/1. Programming in swift/6. Classes/classVSstructChallenge/classVSstructChallenge/main.swift
@@ -0,0 +1,68 @@
+//
+//  main.swift
+//  classVSstructChallenge
+//
+//  Created by KimYong Ho on 14/05/2018.
+//  Copyright © 2018 KimYH. All rights reserved.
+//
+
+import Foundation
+
+class List {
+    let name: String
+    var movieTitle: [String] = []
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func print() {
+        Swift.print("Movie List: \(name)")
+        for movie in movieTitle {
+            Swift.print(movie)
+        }
+        Swift.print("\n")
+    }
+}
+
+class User {
+    var lists: [String: List] = [:]
+    func addList(_ list: List) {
+        lists[list.name] = list
+    }
+
+    func list(forName name: String) -> List? {
+        return lists[name]
+    }
+}
+
+
+let kim = User()
+let lee = User()
+var actionList = List(name: "Melodrama")
+var actionList1 = List(name: "Action")
+
+kim.addList(actionList)
+lee.addList(actionList)
+
+kim.lists["Melodrama"]?.movieTitle.append("Romeo and Juliet")
+kim.lists["Melodrama"]?.movieTitle.append("If only")
+
+lee.lists["Melodrama"]?.movieTitle.append("My love")
+kim.lists["Melodrama"]?.print()
+lee.lists["Melodrama"]?.print()
+
+kim.addList(actionList1)
+lee.addList(actionList1)
+
+kim.lists["Action"]?.movieTitle.append("Captain America")
+lee.lists["Action"]?.movieTitle.append("Guadians of Galaxy")
+
+kim.lists["Action"]?.print()
+lee.lists["Action"]?.print()
+
+//Class or Struct which one will be proper to use?
+//TShirt : Struct
+//User : Class
+//Address : Struct
+//ShoppingCart : Class
