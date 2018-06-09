02da157f3f9ba335e9a1c14bd38d741ec387e92c
Kim Yong Ho <kimyh1981@gmail.com>
2018-06-09 23:30:51 +0900

    Add Checklist app practice.

---
+++ AppPractice/Checklists/Checklists/ChecklistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by KimYong Ho on 07/06/2018.
+//  Copyright © 2018 KimYH. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
