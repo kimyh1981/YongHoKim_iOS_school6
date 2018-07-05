18f1b4d382cdb3e960f5124d2282bc3e61463733
Kim Yong Ho <kimyh1981@gmail.com>
2018-07-05 23:44:49 +0900

    Update practice of checklist app.

---
+++ AppPractice/Checklists/Checklists/AddItemTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AddItemTableViewController.swift
+//  Checklists
+//
+//  Created by KimYong Ho on 05/07/2018.
+//  Copyright © 2018 KimYH. All rights reserved.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
