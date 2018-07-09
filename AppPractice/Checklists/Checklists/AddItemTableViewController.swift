64b06296432f10571a5b50e108fd72b428e920b7
Kim Yong Ho <kimyh1981@gmail.com>
2018-07-09 22:44:45 +0900

    Update practice of checklist app.

--- AppPractice/Checklists/Checklists/AddItemTableViewController.swift
+++ AppPractice/Checklists/Checklists/AddItemTableViewController.swift
@@ -12,6 +12,19 @@ class AddItemTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+    }
+    
+    @IBAction func cancel() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func done() {
+        navigationController?.popViewController(animated: true)
+    }
+    
+    override func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        return nil
     }
 
     
